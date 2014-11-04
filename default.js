326338e1f2fb7159366caae0960a4ced34b67bf5
4bZurd <doggen75@gmail.com>
2014-11-04 12:03:28 +0100

    Update default.js

--- default.js
+++ default.js
@@ -1,3 +1,20 @@
-/* 
-put any js functions here
-*/
+function hideScroll() {
+	document.body.style.overflow = "Hidden";
+	document.getElementById("scrollnorth").style.display = "inline";
+	document.getElementById("scrollsouth").style.display = "inline";
+	document.getElementById("scrolleast").style.display = "inline";
+	document.getElementById("scrollwest").style.display = "inline";
+	}
+	
+var x = 0; /* Definerer posisjon i øst/vest retning (x-aksen) */
+var y = 0; /* Definerer posisjon i nord/sør retning (y-aksen) */
+
+var offsetX = window.width - bredde på kart
+var offsetY = window.height - høyde på kart
+
+function movenorth(multiplier) {
+  if (x >= 0 || x <= offsetX ) {
+		x = (multiplier * 1);
+  }
+  document.getElementById("map").style.top = parseFloat(document.getElementById("map").style.top) + x + "em";
+} 
